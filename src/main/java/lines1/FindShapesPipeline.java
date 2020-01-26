5a13af9eab61a9817ba766feea736eb8c6a7c520
IlyaDzialendzik <46846335+Aquam00ster@users.noreply.github.com>
2020-01-25 18:56:54 -0600

    fixes

--- src/main/java/lines1/FindShapesPipeline.java
+++ src/main/java/lines1/FindShapesPipeline.java
@@ -6,21 +6,19 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.vision.VisionPipeline;
 
 public class FindShapesPipeline implements VisionPipeline {
-    public Mat m;
-
     public FindShapesPipeline()
     {
         NetworkTableInstance.getDefault().startClient("roborio-2773-frc.local");
     } 
     @Override
     public void process(Mat image) {
+        double xc = image.cols() / 2;
         FindShapes.Result result = FindShapes.processImage(image, false);
         //System.out.println(result.contours.length);
         if (result.target != null) {
             double x = result.target[0];
-            double xc = m.cols() / 2;
             double alpha = 30 * (x - xc) / xc;
-            System.out.println("t=" + result.target[0] + "," + result.target[1]);
+            //System.out.println("t=" + result.target[0] + "," + result.target[1]);
             System.out.println(alpha + " Degrees");
             NetworkTableInstance.getDefault().getEntry("/angle").forceSetDouble(alpha);
         }
